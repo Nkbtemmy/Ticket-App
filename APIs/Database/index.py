a308945d66087a3b8f6aaaa36a7d04712ce11234
nkbtemmy <nkubito@augmented-future.com>
2022-12-09 17:42:40 +0200

    trying to dockerlize

--- APIs/Database/index.py
+++ APIs/Database/index.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials,firestore,auth
 
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 database= firestore.client()
 Events = database.collection("Events")
